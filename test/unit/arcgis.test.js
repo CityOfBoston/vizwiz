2063b540a3a57831ae5df55711f28e9c9d742dfa
Corey Oordt <coreyoordt@gmail.com>
2018-03-02 09:21:49 -0600

    Updated tests, removed validators.js and added jest

--- test/unit/arcgis.test.js
+++ test/unit/arcgis.test.js
@@ -2,19 +2,22 @@ import { getServices, ArcGISService } from '../../src/lib/arcgis.js'
 
 describe('ArcGIS API', () => {
   test('Get Services', () => {
-    let services = getServices('https://services.arcgis.com/sFnw0xNflSi8J0uh/arcgis/rest/services')
-    console.log(services)
-    expect(services.length).toBeGreaterThan(1)
+    getServices('https://services.arcgis.com/sFnw0xNflSi8J0uh/arcgis/rest/services').then(services => {
+      expect(services.length).toBeGreaterThan(1)
+    })
   })
   test('ArcGIS Service', () => {
-    let service = new ArcGISService('https://services.arcgis.com/sFnw0xNflSi8J0uh/arcgis/rest/services/food_trucks_schedule/FeatureServer')
-    let layers = service.layers()
-    expect(layers.length).toEqual(1)
+    const service = new ArcGISService('https://services.arcgis.com/sFnw0xNflSi8J0uh/arcgis/rest/services/food_trucks_schedule/FeatureServer')
+    service.layers().then(layers => {
+      expect(layers.length).toEqual(1)
+    })
   })
   test('ArcGIS Layer', () => {
-    let service = new ArcGISService('https://services.arcgis.com/sFnw0xNflSi8J0uh/arcgis/rest/services/food_trucks_schedule/FeatureServer')
-    let layers = service.layers()
-    let fields = layers.fields()
-    expect(fields.length).toEqual(1)
+    const service = new ArcGISService('https://services.arcgis.com/sFnw0xNflSi8J0uh/arcgis/rest/services/food_trucks_schedule/FeatureServer')
+    service.layers().then(layers => {
+      layers[0].fields().then(fields => {
+        expect(fields.length).toEqual(1)
+      })
+    })
   })
 })
