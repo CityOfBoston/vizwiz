8a74f5c9da581fc09f8ab67ee506f3d185024176
Corey Oordt <coreyoordt@gmail.com>
2018-03-20 14:23:00 -0500

    [vizwiz-7] Remove the 'selected' prefix from data attributes and remove most props.

--- src/components/datasource.vue
+++ src/components/datasource.vue
@@ -27,10 +27,9 @@
             <div class="uk-width-1-2"> <!-- column 2 start -->
               <data-connector
                 ref="data-connector"
-                :type="selectedDataSourceType"
-                v-bind='selectedAttributes'
+                :type="dataSourceType"
+                v-bind="attributes"
                 @change="onDataConnectorChange"
-                @isvalid="onDataConnectorIsValid"
               ></data-connector>
             </div> <!-- column 2 -->
           </div> <!-- grid -->
@@ -46,14 +45,14 @@
                     class="uk-input"
                     name="data-legend-label"
                     id="data-legend-label"
-                    v-model="selectedLegendLabel">
+                    v-model="legendLabel">
                 </div>
               </div> <!-- uk-margin -->
 
               <div class="uk-margin">
                 <label class="uk-form-label" for="data-ploygon-style">Polygon Style</label>
                 <div class="uk-form-controls">
-                  <select v-model="selectedPolygonStyle" class="uk-select" id="data-ploygon-style">
+                  <select v-model="polygonStyle" class="uk-select" id="data-ploygon-style">
                     <option disabled value="">Please select one</option>
                     <option :value="key" :key="key" v-for="(key, val) in polygonStyleChoices">{{ val }}</option>
                   </select>
@@ -62,11 +61,10 @@
 
             </div> <!-- column 1 -->
             <div class="uk-width-1-2">
-
               <div class="uk-margin">
                 <label class="uk-form-label" for="data-icon">Icon</label>
                 <div class="uk-form-controls">
-                  <select v-model="selectedIcon" class="uk-select" id="data-icon">
+                  <select v-model="icon" class="uk-select" id="data-icon">
                     <option disabled value="">Please select one</option>
                     <option :value="key" :key="key" v-for="(key, val) in iconChoices">{{ val }}</option>
                   </select>
@@ -76,7 +74,7 @@
                 <label class="uk-form-label" for="data-cluster">
                   <input
                     id="data-cluster"
-                    v-model="selectedClusterPoints"
+                    v-model="clusterPoints"
                     class="uk-checkbox"
                     type="checkbox">
                       Cluster Points
@@ -92,7 +90,7 @@
               <div class="uk-margin">
                 <label class="uk-form-label" for="data-popover">Popover Template</label>
                 <div class="uk-form-controls">
-                  <wysiwyg v-model="selectedPopover" ref="data-popover" id="data-popover"></wysiwyg>
+                  <wysiwyg v-model="popover" ref="data-popover" id="data-popover"></wysiwyg>
                 </div>
               </div> <!-- uk-margin -->
             </div> <!-- column 1 -->
@@ -133,7 +131,7 @@
               </div>
             </div>
             <div class="uk-panel uk-text-center">
-              <img :src="selectedIcon" width="32" height="32">
+              <img :src="icon" width="32" height="32">
             </div>
           </div>
         </li> <!-- preview tab container -->
@@ -189,112 +187,167 @@ export default {
     DataConnector,
   },
   props: {
+    /**
+     * The `uid` of the datasource to retrieve from the store.
+     *
+     * Creates a new `uid` by default.
+     */
     uid: {
       type: String,
       default () { return nanoid() },
     },
-    dataSourceType: {
-      type: String,
-      default () { return store.state.dataSourceTypeChoices['CoB ArcGIS'] }
-    },
-    dataSourceTypeChoices: {
-      type: Object,
-      default () {
-        return store.state.dataSourceTypeChoices
-      }
-    },
-    attributes: {
-      type: Object,
-      default () { return {} }
-    },
-    icon: {
-      type: String,
-      default () { return store.state.iconChoices.default }
-    },
-    iconChoices: {
-      type: Object,
-      default () {
-        return store.state.iconChoices
-      }
-    },
-    clusterPoints: {
-      type: Boolean,
-      default () { return true },
-    },
-    polygonStyle: {
-      type: String,
-      default () { return 'default' }
-    },
-    polygonStyleChoices: {
-      type: Object,
-      default () { return store.state.polygonStyleChoices }
-    },
-    popover: {
-      type: String,
-      default () { return '' }
-    },
-    legendLabel: {
-      type: String,
-      default () { return '' }
-    },
   },
   data () {
     return {
-      selectedDataSourceType: '',
-      selectedAttributes: {},
-      selectedIcon: '',
-      selectedClusterPoints: true,
-      selectedPolygonStyle: '',
-      selectedPopover: '',
-      selectedLegendLabel: '',
+      /**
+       * Are we editing an existing record or adding a new one? Should be
+       * `Edit` or `Add New`
+       */
+      editMode: 'Edit',
+
+      /**
+       * The type of data source. Should be one of `dataSourceTypeChoices`
+       */
+      dataSourceType: '',
+
+      /**
+       * Generic attributes returned from the `data-connector` component.
+       */
+      attributes: {},
+
+      /**
+       * The URL of the selected icon. Should be one of `iconChoices`.
+       */
+      icon: '',
+
+      /**
+       * Should maps using this data source cluster points when zoomed out
+       */
+      clusterPoints: true,
+
+      /**
+       * The name of the polygon style configuration. Should be one of `polygonStyleChoices`
+       */
+      polygonStyle: '',
+
+      /**
+       * The text of the popover template.
+       */
+      popover: '',
+
+      /**
+       * The label to use in the legend of a map that uses this data source.
+       */
+      legendLabel: '',
+
+      /**
+       * The fields of the data source. This is populated by the `data-connector`.
+       * We use these to make it easier for users to insert the fields into the
+       * popover template.
+       */
       fields: [],
-      validateNow: false,
+
+      /**
+       * Which row of the data source are we using to render the sample popover.
+       *
+       * This is here for allowing the user to navigate through several rows
+       * in the sample popover.
+       */
       sampleRow: 1,
+
+      /**
+       * The component to render for connection attributes. Changes based on
+       * the `dataSourceType` choice, and populates `attributes` and `fields`
+       */
       datasourceConnector: null,
+
+      /**
+       * The latest row retrieved from the source for use when rendering the sample popover
+       */
       currentRowCache: {},
     }
   },
   computed: {
-    samplePopover: function () {
-      return templayed(this.selectedPopover)(this.currentRowCache)
-    },
+    /**
+     * Return an representation of this data as an object
+     *
+     * @return     {Object}  This data has an object
+     */
     configObject () {
       return {
         uid: this.uid,
-        type: this.selectedDataSourceType,
-        icon: this.selectedIcon,
-        clusterIcons: this.selectedClusterPoints,
-        polygonStyle: this.selectedPolygonStyle,
-        popover: this.selectedPopover,
-        attributes: Object.assign({}, this.selectedAttributes),
-        legendLabel: this.selectedLegendLabel,
+        dataSourceType: this.dataSourceType,
+        icon: this.icon,
+        clusterPoints: this.clusterPoints,
+        polygonStyle: this.polygonStyle,
+        popover: this.popover,
+        attributes: Object.assign({}, this.attributes),
+        legendLabel: this.legendLabel,
       }
     },
-    editMode () { return 'Edit' },
+
+    /**
+     * Convenience attribute to retrieve the choices of data source types from
+     * the global store.
+     *
+     * @return     {Object}  Data source type choices in the format `{label: value}`
+     */
+    dataSourceTypeChoices () {
+      return store.state.dataSourceTypeChoices
+    },
+
+    /**
+     * Convenience attribute to retrieve the choices of icons from the global store
+     *
+     * @return     {object}  Icon choices in the format `{label: value}`
+     */
+    iconChoices () {
+      return store.state.iconChoices
+    },
+
+    /**
+     * How this item should appear in a listing
+     *
+     * @return     {string}  A brief label for this item
+     */
     label () {
-      if (this.selectedLegendLabel) {
-        return this.selectedLegendLabel
+      if (this.legendLabel) {
+        return this.legendLabel
       }
       let attrs = []
-      if (this.selectedDataSourceType === 'cob-arcgis') {
-        let svc = this.selectedAttributes.service.split('/')
+      if (this.dataSourceType === 'cob-arcgis') {
+        let svc = this.attributes.service.split('/')
         attrs.push(`service: ${svc[svc.length - 2]}`)
-        attrs.push(`layer: ${this.selectedAttributes.layer}`)
+        attrs.push(`layer: ${this.attributes.layer}`)
       } else {
-        for (let prop in this.selectedAttributes) {
-          attrs.push(`${prop}: ${this.selectedAttributes[prop]}`)
-        }
+        for (let prop in this.attributes) {
+          attrs.push(`${prop}: ${this.attributes[prop]}`)
       }
-      return `${this.selectedDataSourceType}: ${attrs.join(', ')}`
     }
+      return `${this.dataSourceType}: ${attrs.join(', ')}`
+    },
+
+    /**
+     * A convenience attribute to retrieve the choices of polygon styles from
+     * the global store
+     *
+     * @return     {object}  Polygon Style choices in the format `{label: value}`
+     */
+    polygonStyleChoices () { return store.state.polygonStyleChoices },
+
+    /**
+     * This renders the popover template with a cached row from the datasource
+     *
+     * @return     {string}  The rendered popover in HTML
+     */
+    samplePopover: function () {
+      return templayed(this.popover)(this.currentRowCache)
+    },
   },
   methods: {
     onInsertField (field) {
       const textArea = this.$refs['data-popover']
       textArea.exec('insertHTML', `{{${field.label}}}`)
-    },
-    onDataSourceTypeChange (event) {
-      console.log('onDataSourceTypeChange')
     },
     onCancel (event) {
       this.$emit('cancel')
@@ -307,7 +360,7 @@ export default {
     },
     onDataConnectorChange (values) {
       this.datasourceConnector = new ArcGISLayer(values.attributes.service, values.attributes.layer)
-      this.selectedAttributes = values.attributes
+      this.attributes = values.attributes
       this.fields = values.fields
       this.datasourceConnector.getRows(this.sampleRow, 1).then(value => {
         let newVal = {}
@@ -334,19 +387,33 @@ export default {
     },
   },
   created () {
-    this.selectedDataSourceType = this.dataSourceType
-    this.selectedAttributes = Object.assign({}, this.attributes)
-    this.selectedIcon = this.icon
-    this.selectedClusterPoints = this.clusterPoints
-    this.selectedPolygonStyle = this.polygonStyle
-    this.selectedPopover = this.popover
-    this.selectedLegendLabel = this.legendLabel
+    if (store.getters['$_datasources/allIds'].indexOf(this.uid) !== -1) {
+      // This is not a new data source. Populate everything from the store
+      const ds = store.getters['$_datasources/getItem'](this.uid)
+      this.dataSourceType = ds.dataSourceType
+      this.attributes = Object.assign({}, ds.attributes)
+      this.icon = ds.icon
+      this.clusterPoints = ds.clusterPoints
+      this.polygonStyle = ds.polygonStyle
+      this.popover = ds.popover
+      this.legendLabel = ds.legendLabel
+    } else {
+      // this is a new data source. Populate everything from defaults
+      this.editMode = 'Add New'
+      this.dataSourceType = store.state.dataSourceTypeChoices['CoB ArcGIS']
+      this.attributes = {}
+      this.icon = store.state.iconChoices.default
+      this.clusterPoints = true
+      this.polygonStyle = 'default'
+      this.popover = ''
+      this.legendLabel = ''
+    }
   },
   validations: {
-    selectedDataSourceType: {required},
-    selectedIcon: {required},
-    selectedPolygonStyle: {required},
-    selectedAttributes: {
+    dataSourceType: {required},
+    icon: {required},
+    polygonStyle: {required},
+    attributes: {
       isValid (value) {
         this.$refs['data-connector'].$v.$reset()
         this.$refs['data-connector'].$v.$touch()
